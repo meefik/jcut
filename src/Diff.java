f3a4aa58cb9e1522249f44736c8afaf2abbc0778
Anton Skshidlevsky <meefik@gmail.com>
2015-06-08 22:48:19 +0300

    Fixed algorithm

--- src/Diff.java
+++ src/Diff.java
@@ -25,15 +25,15 @@ public class Diff {
 
     private static void scan(String root, File path, BufferedWriter bw) throws IOException {
         File[] list = path.listFiles();
-        Arrays.sort(list, new Comparator<File>() {
-            @Override
-            public int compare(File o1, File o2) {
-                return o2.getPath().compareTo(o1.getPath());
-            }
-        });
         if (list == null) {
             return;
         }
+        Arrays.sort(list, new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                return o1.getPath().compareTo(o2.getPath());
+            }
+        });
         for (File f : list) {
             //System.out.println(f.getPath());
             long lastModified = 0;
@@ -62,16 +62,17 @@ public class Diff {
         try (BufferedReader br1 = new BufferedReader(new InputStreamReader(
                 new GZIPInputStream(new FileInputStream(first)), cs));
                 BufferedReader br2 = new BufferedReader(new InputStreamReader(
-                                new GZIPInputStream(new FileInputStream(second)), cs));) {
+                new GZIPInputStream(new FileInputStream(second)), cs));) {
             String line1 = br1 == null ? null : br1.readLine(),
-                    line2 = br2 == null ? null : br2.readLine();
+                   line2 = br2 == null ? null : br2.readLine();
             while (line1 != null && line2 != null) {
                 String[] arr1 = line1.split("\t"), arr2 = line2.split("\t");
-                long hash1 = arr1[0].hashCode(), hash2 = arr2[0].hashCode();
-                if (hash1 == hash2) {
-                    if (arr1[1].hashCode() != arr2[1].hashCode()) {
-                        long time1 = Long.valueOf(arr1[1]),
-                                time2 = Long.valueOf(arr2[1]);
+                String path1 = arr1[0], path2 = arr2[0];
+                long cmp = path1.compareTo(path2);
+                if (cmp == 0) {
+                    long time1 = Long.valueOf(arr1[1]),
+                         time2 = Long.valueOf(arr2[1]);
+                    if (time1 != time2) {
                         // Mod file
                         if (time1 > time2) {
                             System.out.println("mod" + "\t" + line1);
@@ -85,13 +86,13 @@ public class Diff {
                     continue;
                 }
                 // Del file
-                if (hash1 < hash2) {
+                if (cmp < 0) {
                     System.out.println("del" + "\t" + line1);
                     line1 = br1.readLine();
                     continue;
                 }
                 // Add file
-                if (hash1 > hash2) {
+                if (cmp > 0) {
                     System.out.println("add" + "\t" + line2);
                     line2 = br2.readLine();
                     continue;
@@ -99,12 +100,12 @@ public class Diff {
             }
             // Del dir
             while (line1 != null) {
-                System.out.println("del" + "\t" + line1);
+                System.out.println("del1" + "\t" + line1);
                 line1 = br1.readLine();
             }
             // Add dir
             while (line2 != null) {
-                System.out.println("add" + "\t" + line2);
+                System.out.println("add1" + "\t" + line2);
                 line2 = br2.readLine();
             }
         } catch (Exception ex) {
